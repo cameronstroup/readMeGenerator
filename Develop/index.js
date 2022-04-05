cb27f5dca2d9a4b68bbdba27c62624ec86e0f1e9
cameronstroup <90347622+cameronstroup@users.noreply.github.com>
2022-04-04 21:01:53 -0500

    added inquire

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,22 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project? (Required)",
+    validate: (titleInput) => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log("Please enter your title!");
+        return false;
+      }
+    },
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
